17ce774b854f6a703dc1cc655e58ab7e93add328
Lesly Blaise <lpblaise@bhcc.edu>
2022-05-27 10:14:15 -0400

    wiring navigation dependencies

---
+++ app/src/main/java/net/trancool/todoapp/RosterListFragment.kt
@@ -0,0 +1,20 @@
+package net.trancool.todoapp
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class RosterListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+   ): View?  = inflater.inflate(R.layout.todo_roster, container, false)
+//    {
+//        return super.onCreateView(inflater, container, savedInstanceState)
+//    }
+
+
+}
